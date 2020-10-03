cf3972d21d3460c67fe0028af063475b1da77648
Akash <akashsinhakvs@gmail.com>
2020-10-03 13:49:25 +0530

    emoji uodate

--- commands/roles.js
+++ commands/roles.js
@@ -7,7 +7,7 @@ module.exports.run = async (ErrorBot, message, argument) => {
     message.channel.send(roles_message_embed).then(sentEmbed => {
       sentEmbed.react("761213162866868256")
       sentEmbed.react("761213162665017377")
-      sentEmbed.react("761213163294162985")
+      sentEmbed.react("761863756320997376")
     })
   }
 };
