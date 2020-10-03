c28206e912b7be16977a10706fb70479fff8adf8
Akash <akashsinhakvs@gmail.com>
2020-10-03 13:58:23 +0530

    emoji change

--- commands/roles.js
+++ commands/roles.js
@@ -5,9 +5,9 @@ module.exports.run = async (ErrorBot, message, argument) => {
   if (message.member.hasPermission('ADMINISTRATOR')){
     const roles_message_embed = 'Hi there, Take the roles of games you play\n<:valorant:761149464152637471> => Valorant\n<:amongus:761149464928190464> => Amoung Us\n<:csgo:761155171446554657> => Counter Strike ; Go';
     message.channel.send(roles_message_embed).then(sentEmbed => {
-      sentEmbed.react("761213162866868256")
-      sentEmbed.react("761213162665017377")
-      sentEmbed.react("761863756320997376")
+      sentEmbed.react("761866568841035816")
+      sentEmbed.react("761866569155346442")
+      sentEmbed.react("761866569051537428")
     })
   }
 };
