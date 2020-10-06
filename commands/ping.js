3bc1b299cd60e199b93c54b825ed04fa84cf16dd
Akash <akashsinhakvs@gmail.com>
2020-10-06 15:18:19 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,11 +3,11 @@ const empty = require('is-empty');
 
 module.exports.run = async(ErrorBot, message, argument) => {
   if(empty(argument)){
-    message.channel.send("Pinging...").then(m =>{
-      var ping = m.createdTimestamp - message.createdTimestamp;
+    message.channel.send("Pinging...").then(msg =>{
+      var ping = msg.createdTimestamp - message.createdTimestamp;
       var apiPing = Math.round(ErrorBot.ws.ping);
 
-      m.edit(`**Bot is online**\nBot API ping is ${apiPing}ms\nMessage ping is ${ping}ms`);
+      msg.edit(`**Bot is online**\nBot API ping is ${apiPing}ms\nMessage ping is ${ping}ms`);
     });
   }
 };
